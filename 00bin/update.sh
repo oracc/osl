d5499d178ab3e307f82e7994ffd50f29b0feece9
Steve Tinney <stinney@upenn.edu>
2019-05-11 07:46:36 -0400

    update oid data on my dev machine in 00bin/update.sh

--- 00bin/update.sh
+++ 00bin/update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+(cd ~/orc/oid ; scp -r build:/home/oracc/oid/* .)
 serve-install.sh ogsl build-oracc.museum.upenn.edu
 serve-install.sh ogsl/signlist build-oracc.museum.upenn.edu
 serve-index.sh ogsl build-oracc.museum.upenn.edu
