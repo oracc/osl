8aba7d90d77f89c16904598016d104ed05d0d79b
stinney <stinney@upenn.edu>
2023-07-10 11:26:49 +0000

    set +r perms on mac because tar x doesn't seem to do that

--- 00bin/update.sh
+++ 00bin/update.sh
@@ -6,3 +6,7 @@ serve-install.sh ogsl/signlist build-oracc.museum.upenn.edu
 serve-index.sh ogsl build-oracc.museum.upenn.edu
 serve-index.sh ogsl/signlist build-oracc.museum.upenn.edu
 chmod -w 00lib/ogsl.asl
+if [ -d /Users ]; then
+    cd 02www
+    sudo chmod -R o+r *
+fi
