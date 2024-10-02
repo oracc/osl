2d034ebecdc7c7757f4d2e044a6c9169afcc9b33
Steve Tinney <stinney@upenn.edu>
2024-10-02 02:58:55 +0000

    sxw-pua.pos tweak but this is going to change again to XML format xincluded data probs

--- 00bin/sxw-pua.sh
+++ 00bin/sxw-pua.sh
@@ -5,5 +5,5 @@
 
 puablocks=`grep Xsux 00etc/oracc-pua.tab | cut -f5 | sort -u`
 for a in $puablocks ; do
-    grep Xsux 00etc/oracc-pua.tab | grep $a'$' | cut -f1 >00etc/sws-pos-$a.txt
+    grep Xsux 00etc/oracc-pua.tab | grep $a'$' | cut -f1 >00etc/sxw-pos-$a.txt
 done
