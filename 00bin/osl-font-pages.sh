6d8597f51053c18d057862c2a721cd8550d5a5e5
Steve Tinney <stinney@upenn.edu>
2024-11-04 20:15:22 +0000

    refactor fonts part of portal

--- 00bin/osl-font-pages.sh
+++ 00bin/osl-font-pages.sh
@@ -2,3 +2,5 @@
 00bin/osl-one-font-page.sh Oracc-LAK ofs-lak
 00bin/osl-one-font-page.sh Oracc-gudea ofs-gudea
 00bin/osl-one-font-page.sh Oracc-OBF-Regular ofs-oobf
+00bin/osl-one-font-page.sh Oracc-Noto-SansCuneiform ofs-noto
+00bin/osl-one-font-page.sh Oracc-NA-Outline ofs-nao
