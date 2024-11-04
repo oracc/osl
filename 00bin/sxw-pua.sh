1fab8cfdd12773730d6519cc93b24ec7d1b450a7
Steve Tinney <stinney@upenn.edu>
2024-11-04 23:31:43 +0000

    extending oracc-pua.tab to support notes column

--- 00bin/sxw-pua.sh
+++ 00bin/sxw-pua.sh
@@ -5,5 +5,6 @@
 
 puablocks=`grep Xsux 00etc/oracc-pua.tab | cut -f5 | sort -u`
 for a in $puablocks ; do
-    grep Xsux 00etc/oracc-pua.tab | grep $a'$' | cut -f1 >00etc/sxw-pos-$a.txt
+    grep Xsux 00etc/oracc-pua.tab | grep '	'$a'	' | cut -f1,6 | \
+	rocox -h -R'<oid xml:id="%1"><td class="notes">%2</td></oid>' > 00etc/sxw-pos-$a.xml
 done
