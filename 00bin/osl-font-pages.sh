2d1acf436ad68415cd25e93e8884841d4b21b42a
Stephen Tinney <stinney@upenn.edu>
2024-11-15 20:49:30 -0500

    more RSP fixes

--- 00bin/osl-font-pages.sh
+++ 00bin/osl-font-pages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 00bin/osl-one-font-page.sh Oracc-LAK ofs-lak
+00bin/osl-one-font-page.sh Oracc-RSP ofs-rsp
 00bin/osl-one-font-page.sh Oracc-gudea ofs-gudea
 00bin/osl-one-font-page.sh Oracc-OBF-Regular ofs-oobf
 00bin/osl-one-font-page.sh Oracc-Noto-SansCuneiform ofs-noto
