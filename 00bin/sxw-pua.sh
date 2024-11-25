e002d18a25c9d4580e1eb5b1460e709a3335737d
Steve Tinney <stinney@upenn.edu>
2024-11-25 21:48:48 +0000

    add script into pua-tables

--- 00bin/sxw-pua.sh
+++ 00bin/sxw-pua.sh
@@ -5,6 +5,6 @@
 
 puablocks=`cat 00etc/oracc-pua.tab | cut -f5 | sort -u`
 for a in $puablocks ; do
-    cat 00etc/oracc-pua.tab | grep '	'$a'	' | cut -f1,6 | \
-	rocox -h -R'<oid xml:id="%1"><td class="notes">%2</td></oid>' > 00etc/sxw-pos-$a.xml
+    cat 00etc/oracc-pua.tab | grep '	'$a'	' | cut -f1,4,6 | \
+	rocox -h -R'<oid xml:id="%1" script="%2"><td class="notes">%3</td></oid>' > 00etc/sxw-pos-$a.xml
 done
